c0cd1d7e336d27c128a39a47a4ac37b2613e5f41
Adam Allevato <Kukanani@users.noreply.github.com>
2018-11-16 15:18:29 -0600

    Huge refactor to get rid of old c6 stuff and improve var names

--- src/data_logger_bag/logger.py
+++ src/data_logger_bag/logger.py
@@ -29,11 +29,10 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # Author: Vivian Chu, vchu@gatech.edu
+# Modified: Adam Allevato, allevato@utexas.edu, November 2018
 
 '''
-logger.py
-
-Base logger class that controls calling the rosbag command to write to bag files.
+Calls the command to write to bag files.
 Allows for specification of where to store the bag files to as well as
 the topics to record
 '''
@@ -47,230 +46,201 @@ from std_msgs.msg import Bool, String
 from data_logger_bag.msg import LogControl
 from data_logger_bag.srv import GetSettingsResponse, GetSettings
 
-class BagDataLogger:
-
-    REQUEST_LOGGER_SETTING_SRV = "get_data_logger_settings"
-    GLOBAL_CONTROL_TOPIC = "/data_logger_bag/control"
-    GLOBAL_START_STOP_TOPIC = "/data_logger_bag/flag_topic"
-
-    ''' 
-    Initialize the node 
+def terminate_ros_node(s):
     '''
+    Useful function from ROS answers that kills all nodes with the string match
+    at the beginning
+    '''
+    list_cmd = subprocess.Popen("rosnode list", shell=True, stdout=subprocess.PIPE)
+    list_output = list_cmd.stdout.read()
+    retcode = list_cmd.wait()
+    assert retcode == 0, "List command returned %d" % retcode
+    for str in list_output.split("\n"):
+        if (str.startswith(s)):
+            os.system("rosnode kill " + str)
+
+def ensure_dir(d):
+    '''
+    Simple function that creates all the directories in a path if they
+    don't exist.
+
+    d(string): the fully-qualified (no ~, etc.) directory path to
+               check for existence. Note that this is a directory path,
+               not a filename.
+    '''
+    if not os.path.exists(d):
+        os.makedirs(d)
+
+class DataLoggerBag:
+    LOGGER_START_STOP_TOPIC = "~set_logging"
+    GET_SETTINGS_SRV= "~get_settings"
+    SET_SETTINGS_TOPIC= "~set_settings"
+    OUTPUT_FILEPATH_TOPIC = "~last_bag_file_path"
+
+    RECORD_TOPICS_PARAM = "~record_topics"
+    LOG_DIRECTORY_PARAM = "~log_directory"
+
     def __init__(self):
+        ''' 
+        Initialize the ROS node, subscribers, and services, and load parameters
+        '''
 
-        # Initialize node
-        rospy.init_node("data_logger", anonymous=True)
+        ###
+        # Initialize variables
+        rospy.init_node("data_logger_bag", anonymous=True)
 
+
+        self.is_logging = False
+        self.log_prefix = ""
+        self.log_directory = ""
+        self.current_bag_filename = ""
+
+        ###
+        # Set up ROS pubs/subs/services/etc.
+
+        # Load parameters
         # default topics to record - separated only by a space
-        default_record_topics = "joint_states c6_logger_flag"
-        default_log_flag = "data_logger_flag"
-        default_data_location = "data"
-        default_data_prefix = "playback"
-        default_c6_task_topic = "C6_Task_Description"
-
-        # Get parameter names from the param server
-        self.c6_task_topic = rospy.get_param("~c6_task_topic", default_c6_task_topic) 
-        self.logger_flag_topic = rospy.get_param("~log_flag_topic", default_log_flag)
-        self.record_topics = rospy.get_param("~record_topics", default_record_topics)
-        self.data_location = rospy.get_param("~datapath", default_data_location)
-        self.data_prefix = rospy.get_param("~data_prefix", default_data_prefix)
-
-        # Set the global topic name
-        rospy.set_param(self.GLOBAL_CONTROL_TOPIC, self.c6_task_topic)
-        rospy.set_param(self.GLOBAL_START_STOP_TOPIC, self.logger_flag_topic)
-
-        # Initialize the flag to false
-        self.logger_flag = False
-
-        # Intialize default task and skill names
-        self.task = "default_task"
-        self.skill = "default_skill"
-
-        # Initialize some default runName
-        self.runName = "";
-        self.filename = "";
-
-        # Append the task and skill onto the data_location
-        self.data_custom_location = os.path.join(self.data_location, self.task, self.skill)
+        default_record_topics = ""
+        default_log_directory = "~/data_logger_bag"
+        self.record_topics = rospy.get_param(self.RECORD_TOPICS_PARAM, default_record_topics).split(" ")
+        log_directory = rospy.get_param(self.LOG_DIRECTORY_PARAM, default_log_directory)
+        self.set_log_directory(log_directory)
 
         # Setup publisher for bagfile
-        self.bag_file_loc_pub = rospy.Publisher('bag_file_loc', String, queue_size=10)
+        self.bag_file_loc_pub = rospy.Publisher(self.OUTPUT_FILEPATH_TOPIC, String, queue_size=1)
 
         # Setup service for getting the current settings
-        self.setting_srv = rospy.Service(self.REQUEST_LOGGER_SETTING_SRV, GetSettings, self._return_settings)
-
-        rospy.loginfo("Initialized data logger node with logger flag topic: %s" % self.logger_flag_topic)
-
-    '''
-    Setup service response to getting the current settings of the logger
-    '''
-    def _return_settings(self, req):
-
-        response = GetSettingsResponse()
-        msg = LogControl()
-        msg.taskName = self.task
-        msg.skillName = self.skill
-        msg.actionType = self.data_prefix
-        msg.topics = self.record_topics
-        msg.runName = self.runName
-        response.response = msg
-        response.data_location = os.path.join(os.path.expanduser("~"),self.data_custom_location)
-        response.filename = self.filename
-        return response
-
-    '''
-    Setup the subscribers
-    '''
-    def startListener(self):
-
-        # We do this after we init to not have issues with messages being listen to before we are ready
-        # Listen for the flag to know when to start and stop the recording
-        rospy.Subscriber(self.logger_flag_topic, Bool, self.flag_callback)
+        self.setting_srv = rospy.Service(self.GET_SETTINGS_SRV, GetSettings, self.cb_get_settings)
 
         # Setup a listener for the task and action to put in a custom folder
         # This message also contains which logging topics that the user can
         # specify to listen to
-        rospy.Subscriber(self.c6_task_topic, LogControl, self.change_log_settings_cb)
+        rospy.Subscriber(self.SET_SETTINGS_TOPIC, LogControl, self.cb_set_settings)
 
-        # Start the ros node
+        # Listen for the flag to know when to start and stop the recording
+        rospy.Subscriber(self.LOGGER_START_STOP_TOPIC, Bool, self.cb_set_logging)
+
+        ###
+        # Ready to rumble
+        rospy.loginfo("Initialized data logger bag node.")
         rospy.spin()
 
 
-    '''
-    CB that changes the location of where we are saving data to based off of 
-    the current task and action
-    '''
-    def change_log_settings_cb(self, msg):
+    def cb_get_settings(self, req):
+        '''
+        Setup service response to getting the current settings of the logger
+        '''
+        response = GetSettingsResponse()
+        settings = LoggerSettings()
+        settings.log_directory = self.log_directory
+        settings.log_prefix = self.log_prefix
+
+        response.current_settings = settings
+        return response
+
+
+    def cb_set_settings(self, msg):
+        '''
+        CB that changes the location of where we are saving data to based off of 
+        the current task and action
+        '''
 
         # Only change the directory if we are not currently writing to a file
-        if self.logger_flag is False:
-    
-            if msg.taskName is not "":
-                self.task = msg.taskName
 
-            if msg.skillName is not "":
-                self.skill = msg.skillName
+        if self.is_logging:
+            rospy.logwarn("Currently recording, ignoring settings change.")
+            return
 
-            if msg.topics is not "":
-                self.record_topics = msg.topics
-        
-            if msg.runName is not "":
-                self.runName = msg.runName
-                rospy.loginfo("Filename to be written is: %s" % self.runName)
-    
-            playback_flag = msg.playback
+        else:
+            self.is_logging = True
+
+            if msg.record_topics:
+                self.record_topics = msg.record_topics
+            
+            if msg.log_prefix is not "":
+                self.log_prefix = msg.log_prefix
+
+            self.set_log_directory(msg.log_directory)
 
             # Append the task and skill onto the data_location
-            self.data_custom_location = os.path.join(self.data_location, self.task, self.skill) 
             rospy.loginfo("Location writing changed to: %s" % self.data_custom_location)
 
             # What topics we're recording
             rospy.loginfo("Topics that will be subscribed to: %s" % self.record_topics)
 
-            # Change prefix of the data to say if we are demonstrating or playing back
-            if (playback_flag):
-                self.data_prefix="playback"
-            else:
-                self.data_prefix="learning"
 
-            rospy.loginfo("Data prefix is: %s" % self.data_prefix)
-
-        else:
-            rospy.loginfo("Currently still writing previous record. Settings for logging NOT changed")
-
-
-    '''
-    Callback for when to trigger recording of bag files
-    '''
-    def flag_callback(self, msg):
+    def cb_set_logging(self, msg):
+        '''
+        Callback for when to trigger recording of bag files
+        '''
 
         # Setup flag from message
         rospy.loginfo(rospy.get_name() + ": I heard %s" % msg.data) 
        
         # Checks for change in flag first
-        if self.logger_flag != msg.data:
-            
-            rospy.loginfo("Detected flag state change")
-
+        if self.is_logging != msg.data:
             # Set the flag after checking 
-            self.logger_flag = msg.data
+            self.is_logging = msg.data
 
             # Then check what the message is telling us
-            if msg.data is True:
+            if msg.data:
                 self.startRecord()
             else:
                 self.stopRecord()
+        elif msg.data:
+            rospy.logwarn("recording requested, but already recording")
+        else:
+            rospy.logwarn("stop recording requested, but not recording")
 
 
+    def set_log_directory(self, log_directory):
+        """
+        Do basic validation and set the logging directory.
+        """
+
+        if log_directory is not "":
+            self.log_directory = \
+                os.path.abspath(
+                    os.path.expanduser(
+                        os.path.expandvars(
+                            log_directory
+                        )
+                    )
+                )
+            ensure_dir(log_directory)
 
     def startRecord(self):
-    
-        rospy.loginfo("Set up bag file to write to")
-        if self.runName is "":
-            filename = self.data_prefix + "_"+time.strftime("%Y-%m-%dT%H%M%S") + ".bag"
-        else:
-            filename = self.data_prefix + "_"+self.runName+"_"+time.strftime("%Y-%m-%dT%H%M%S") + ".bag"
-        # Store the current filename
-        self.filename = filename
-
-        self.datapath = os.path.join(os.path.expanduser("~"),self.data_custom_location, filename)
-        #rospy.loginfo("File name is: %s" % datapath)
+        # construct the fully qualified filename from the log directory, prefix, and time.
+        prefix = ""
+        if self.log_prefix != "":
+            prefix = self.log_prefix + "_"
+        self.current_bag_filename = os.path.join(self.log_directory, prefix + time.strftime("%Y-%m-%dT%H%M%S") + ".bag")
         
-        # Check if directory exists
-        self.ensure_dir(self.datapath)
+        # Check if directory exists. We have to do this after constructing the filename, because
+        # the parent directory we have to check depends on both the log directory and the prefix,
+        # which may include additional folders.
+        bag_directory = os.path.dirname(self.current_bag_filename)
+        rospy.loginfo("ensuring that {} exists..".format(bag_directory))
+        ensure_dir(bag_directory)
        
         # Setup the command for rosbag
         # We don't use the compression flag (-j) to avoid slow downs
-        rosbag_cmd = " ".join(("rosbag record -O",self.datapath,self.record_topics))
-        rospy.loginfo("Command to run: %s" % rosbag_cmd)
+        rosbag_cmd = " ".join(("rosbag record -O", self.current_bag_filename, " ".join(self.record_topics)))
 
         # Start the command through the system
+        rospy.loginfo("Running the following record command: %s" % rosbag_cmd)
         self.rosbag_proc = subprocess.Popen([rosbag_cmd], shell=True)
-        rospy.loginfo("Recording bag file")
-        
 
     def stopRecord(self):
-
         # Send command to the process to end (same as ctrl+C)
         self.rosbag_proc.send_signal(subprocess.signal.SIGINT)
         rospy.loginfo("Stopping bag file recording")
 
         # Kill all extra rosbag "record" nodes
-        self.terminate_ros_node("/record")
+        terminate_ros_node("/record")
 
         # Publish out what bag file that we finished reccording to
-        self.bag_file_loc_pub.publish(String(self.datapath))
-
-    '''
-    Useful function from ROS answers that kills all nodes with the string match
-    at the beginning
-    '''
-    def terminate_ros_node(self,s):
-        list_cmd = subprocess.Popen("rosnode list", shell=True, stdout=subprocess.PIPE)
-        list_output = list_cmd.stdout.read()
-        retcode = list_cmd.wait()
-        assert retcode == 0, "List command returned %d" % retcode
-        for str in list_output.split("\n"):
-            if (str.startswith(s)):
-                os.system("rosnode kill " + str)
-
-
-
-    '''
-    Simple function that makes sure the data directory specified exists
-    '''
-    def ensure_dir(self, f):
-        d = os.path.dirname(f)
-        if not os.path.exists(d):
-            os.makedirs(d)
+        self.bag_file_loc_pub.publish(String(self.current_bag_filename))
 
 if __name__ == '__main__':
-
-    data_logger = BagDataLogger()
-    data_logger.startListener()
-
-
-
-
-
-
+    dlb = DataLoggerBag()
